a81e96f8624bc1103ce2f3f8220a2094a50a069f
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-09-29 20:28:14 +0530

    Algorithm for mistakes
    Worked on the algorithm for mistakes.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const typingText = document.querySelector(".typing-text p");
 inpField = document.querySelector(".wrapper .input-field");
+mistakeTag = document.querySelector(".mistake span");
 
-let charIndex = 0;
+let charIndex = mistakes = 0;
 
 function randomParagraph() {
     // getting random number and it'll always less than the paragraphs length
@@ -20,24 +21,29 @@ function randomParagraph() {
 function initTyping() {
     const characters = typingText.querySelectorAll("span")
     let typedChar = inpField.value.split("")[charIndex];
+    //if user hasn't entered any character or pressed backspace
     if (typedChar == null) {
-        //if user hasn't entered any character or pressed backspace
-        charIndex--;
+        charIndex--; //decrement charIndex
+        //decrement mistakes only if the charIndex span contains incorrect class
+        if (characters[charIndex].classList.contains("incorrect")) {
+            mistakes--;
+        }
         characters[charIndex].classList.remove("correct", "incorrect");
     } else {
         if (characters[charIndex].innerText === typedChar) {
             // if user typed character and shown character matched then add the
-            // correct class else add the incorrect class
+            // correct class else increment the mistakes and add the incorrect class
             characters[charIndex].classList.add("correct");
         } else {
+            mistakes++;
             characters[charIndex].classList.add("incorrect");
         }
         charIndex++; // increment charIndex either user typed correct or incorrect character
     }
-
-
     characters.forEach(span => span.classList.remove("active"));
     characters[charIndex].classList.add("active");
+
+    mistakeTag.innerText = mistakes;
 }
 
 randomParagraph();
